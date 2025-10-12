c52af3bdd215ed89f8acafa52edfbedb4c5dfaf5
Jedad11 <jedsadice@gmail.com>
2025-10-12 22:01:42 +0700

    crud staff  done

--- backend/src/routes/staff.route.ts
+++ backend/src/routes/staff.route.ts
@@ -4,7 +4,7 @@ import * as StaffController from "../controllers/staff.controller"
 const staffRouter = express.Router()
 
 staffRouter.get("/staffs", StaffController.getAllStaff)
-staffRouter.put("/update", StaffController.updateStaff)
-staffRouter.delete("/delete", StaffController.deleteStaff)
+staffRouter.put("/update/:id", StaffController.updateStaff)
+staffRouter.delete("/delete/:id", StaffController.deleteStaff)
 
 export default staffRouter
